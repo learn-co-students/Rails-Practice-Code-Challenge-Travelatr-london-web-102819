b9be7cec69523b216985e20d418277db8402f67d
PianoFF <Fangfei1star@gmail.com>
2019-11-27 11:01:58 +0000

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -24,6 +24,15 @@ class Blogger < ApplicationRecord
         # byebug
         self.posts.group_by{|post| post.destination}.sort.take(5)
         #self.posts.group_by(&:destination).sort_by{|d| d.size}
+        
+        #more ways to do this:
+        #destination_count=Hash.new(0)
+            #a new hash with default value to 0
+        #self.posts.destinations.each{|destination|destination_count[destination]+=1}
+            #iterating over all blogger's destinations for posts, keep a counter of each
+            #restuling destination_count = {"destination"=>1, etc}
+        #destination_count.sort_by{|destination, count| count}.reverse.take(5)
+            #hash's sorty_by method, and reverse so it's descending, and take the first 5
     end 
     
     
